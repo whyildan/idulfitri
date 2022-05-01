b25ea71d29933b412989d3ef5256c666d1bad517
whyldan <45098368+whyldan@users.noreply.github.com>
2022-05-02 00:17:12 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,47 +1,47 @@
-// var makeItRain = function() {
-//   //clear out everything
-//   $('.rain').empty();
+var makeItRain = function() {
+//clear out everything
+$('.rain').empty();
 
-//   var increment = 0;
-//   var drops = "";
-//   var backDrops = "";
+var increment = 0;
+var drops = "";
+var backDrops = "";
 
-//   while (increment < 100) {
+while (increment < 100) {
 //     //couple random numbers to use for various randomizations
 //     //random number between 98 and 1
-//     var randoHundo = (Math.floor(Math.random() * (98 - 1 + 1) + 1));
+var randoHundo = (Math.floor(Math.random() * (98 - 1 + 1) + 1));
 //     //random number between 5 and 2
-//     var randoFiver = (Math.floor(Math.random() * (5 - 2 + 1) + 2));
+ var randoFiver = (Math.floor(Math.random() * (5 - 2 + 1) + 2));
 //     //increment
-//     increment += randoFiver;
+ increment += randoFiver;
 //     //add in a new raindrop with various randomizations to certain CSS properties
-//     drops += '<div class="drop" style="left: ' + increment + '%; bottom: ' + (randoFiver + randoFiver - 1 + 100) + '%; animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"><div class="stem" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div><div class="splat" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div></div>';
-//     backDrops += '<div class="drop" style="right: ' + increment + '%; bottom: ' + (randoFiver + randoFiver - 1 + 100) + '%; animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"><div class="stem" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div><div class="splat" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div></div>';
-//   }
+drops += '<div class="drop" style="left: ' + increment + '%; bottom: ' + (randoFiver + randoFiver - 1 + 100) + '%; animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"><div class="stem" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div><div class="splat" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div></div>';
+backDrops += '<div class="drop" style="right: ' + increment + '%; bottom: ' + (randoFiver + randoFiver - 1 + 100) + '%; animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"><div class="stem" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div><div class="splat" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div></div>';
+}
 
-//   $('.rain.front-row').append(drops);
-//   $('.rain.back-row').append(backDrops);
-// }
+$('.rain.front-row').append(drops);
+$('.rain.back-row').append(backDrops);
+}
 
-// $('.splat-toggle.toggle').on('click', function() {
-//   $('body').toggleClass('splat-toggle');
-//   $('.splat-toggle.toggle').toggleClass('active');
-//   makeItRain();
-// });
+$('.splat-toggle.toggle').on('click', function() {
+$('body').toggleClass('splat-toggle');
+$('.splat-toggle.toggle').toggleClass('active');
+makeItRain();
+});
 
-// $('.back-row-toggle.toggle').on('click', function() {
-//   $('body').toggleClass('back-row-toggle');
-//   $('.back-row-toggle.toggle').toggleClass('active');
-//   makeItRain();
-// });
+$('.back-row-toggle.toggle').on('click', function() {
+$('body').toggleClass('back-row-toggle');
+$('.back-row-toggle.toggle').toggleClass('active');
+makeItRain();
+});
 
-// $('.single-toggle.toggle').on('click', function() {
-//   $('body').toggleClass('single-toggle');
-//   $('.single-toggle.toggle').toggleClass('active');
-//   makeItRain();
-// });
+$('.single-toggle.toggle').on('click', function() {
+$('body').toggleClass('single-toggle');
+ $('.single-toggle.toggle').toggleClass('active');
+ makeItRain();
+ });
 
-// makeItRain();
+makeItRain();
 
 new Typed('#typed',{
         strings : ["Selamat Hari Raya Idul Fitri 1443 H.", "Mohon Maaf Lahir dan Batin.", "Kami sekeluarga mohon maaf apabila ada salah dalam bertutur kata", "Maupun ada perilaku kami yang kurang berkenan.", "Semoga Allah memenuhi hari yang fitri ini dan selanjutnya dengan rezeki yang berlimpah, ketenangan batin, dan perkembangan karier yang gemilang.", "Mohon Maaf Lahir & Batin from - Keluarga Bapak Sigit"],
