dbb376a29af813ff1dcb47931d414e65f8612878
whyldan <45098368+whyldan@users.noreply.github.com>
2022-05-02 00:13:51 +0700

    Update script.js

--- script.js
+++ script.js
@@ -44,7 +44,7 @@
 // makeItRain();
 
 new Typed('#typed',{
-        strings : ["Selamat Hari Raya Idul Fitri 1441 H.", "Mohon Maaf Lahir dan Batin.", "Saya mohon maaf apabila ada salah dalam bertutur kata", "Maupun ada perilaku saya yang kurang berkenan.", "Semoga kemeriahan tetap terasa walaupun ditengah pandemi.", "Mohon Maaf Lahir & Batin from - Whyldan"],
+        strings : ["Selamat Hari Raya Idul Fitri 1443 H.", "Mohon Maaf Lahir dan Batin.", "Kami sekeluarga mohon maaf apabila ada salah dalam bertutur kata", "Maupun ada perilaku kami yang kurang berkenan.", "Semoga Allah memenuhi hari yang fitri ini dan selanjutnya dengan rezeki yang berlimpah, ketenangan batin, dan perkembangan karier yang gemilang.", "Mohon Maaf Lahir & Batin from - Keluarga Bapak Sigit"],
         typeSpeed : 70,
         delaySpeed : 70,
         loop : false
